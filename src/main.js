5a574334f9eb1ee8eea0b56924b4823b64dd5b87
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-10-26 15:27:35 -0500

    got vite working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router.js";
-import vuetify from "./plugins/vuetify.js";
+import vuetify from "../plugins/vuetify.js";
 
-createApp(App).use(vuetify).use(router).mount("#app");
+createApp(App).use(vuetify).mount("#app");

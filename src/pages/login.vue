98c64d54f64bd5ff88fe5fa50fc7ebaaf0d47523
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-10-26 17:13:14 -0500

    wip

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,15 +2,41 @@
 
 <template>
     <v-container class="fill-height d-flex justify-center align-center">
-    <v-card style="text-align: center" 
-    width="20%" 
-    height="40%"
-    >
-      <v-card-text>Login</v-card-text>
+    <v-card class="login-card">
+      <v-card-text class="title">Get Started!</v-card-text>
+      <!-- FIX THIS -->
+      <v-col class="d-flex flex-column" style="gap: 10px; justify-content: center;">
+          <v-btn class="buttons" variant="tonal">Login</v-btn>
+          <v-btn class="buttons" variant="tonal">Login as Admin</v-btn>
+    </v-col>
     </v-card>
   </v-container>
 </template>
 
 <style scoped>
-    
+    /* .column {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        justify-content: center;
+    } */
+
+    .login-card {
+        background-color: #f2f2f2;
+        text-align: center;
+        width: 30%;
+        height: 50%;
+        border-radius: 40px;
+    }
+
+    .title {
+        padding-top: 40%;
+        font-size: 26px;
+        font-weight: 600;
+    }
+
+    .buttons {
+        width: 300px;
+        
+    }
 </style>

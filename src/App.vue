04f1dbec47a108359bfb4f8e93f165f05a96ea1e
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-10-26 15:43:08 -0500

    wip

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,8 @@
 
 <template>
   <v-app>
-    <MenuBar :key="$route.fullPath" />
     <v-main>
-      <router-view />
+
     </v-main>
   </v-app>
 </template>
---
+++ src/pages/login.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    test text
+    <div class="login-container">
+        test
+    </div>
+</template>
+
+<style scoped>
+    /* .login-container {
+
+    } */
+</style>

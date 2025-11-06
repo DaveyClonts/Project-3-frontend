5978995787d1f3baaf87629b2ad8ed2e735fd625
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-06 12:41:39 -0600

    first commit

--- src/pages/coach/dashboard-coach.vue
+++ src/pages/coach/dashboard-coach.vue
@@ -1,6 +1,37 @@
 <template>
-    Dashboard View
+    <div class="page-margins">
+        <v-col class="fill-height flex-grow-0" style="width: 800px">
+            <v-row>
+                <div class="title-text">
+                    Welcome, Coach Jones
+                </div>
+            </v-row>
+            <v-row>
+                <v-card width="800px">Test</v-card>
+            </v-row>
+            <v-row>
+                <v-card>Test</v-card>
+            </v-row>
+        </v-col>
+        <v-col>
+            <v-card></v-card>
+        </v-col>
+    </div>
 </template>
+
+<style scoped>
+
+.page-margins {
+    Padding: 40px;
+    height: 100vh;
+}
+
+.title-text {
+    font-size: 30px;
+    font-weight: 600;
+}
+
+</style>
 
 <script setup>
     

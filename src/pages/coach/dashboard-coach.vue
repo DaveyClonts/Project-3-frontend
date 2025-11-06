94d77ff62d878b8b4a1fd6764a327fb4c56cddec
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-06 15:44:50 -0600

    smaller navbar

--- src/pages/coach/dashboard-coach.vue
+++ src/pages/coach/dashboard-coach.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="page-margins">
-        <v-col class="fill-height flex-grow-0" style="width: 800px">
+        <v-col class="fill-height flex-grow-0" style="width: 70vw">
             <v-row>
                 <div class="title-text">
                     Welcome, Coach Jones
                 </div>
             </v-row>
             <v-row>
-                <v-card width="800px">Test</v-card>
+                <v-card class="card">Test</v-card>
             </v-row>
             <v-row>
                 <v-card>Test</v-card>
@@ -29,6 +29,14 @@
 .title-text {
     font-size: 30px;
     font-weight: 600;
+}
+
+.card {
+    margin-top: 25px;
+    margin-bottom: 50px;
+    border-radius: 20px;
+    width: 70vw;
+    height: 30vh;
 }
 
 </style>

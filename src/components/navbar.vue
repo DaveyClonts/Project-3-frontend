1b077ee72f599eb2f453870babc8442f0a1014ea
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-10 11:03:50 -0600

    logout popup

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,29 +1,20 @@
 <template>
-    <topbar>
-        <div class="navbar">
-            <ul class="nav-list">
-                <li
-                    v-for="item in navItems"
-                    :key="item.name"
-                    class="nav-item"
-                    @click="navigate(item)"
-                    :class="{ active: route.name === item.name }"
-                >
-                    {{ item.label }}
-                </li>
-            </ul>
-        </div>
-    </topbar>
+    <div class="navbar">
+        <ul class="nav-list">
+            <li
+                v-for="item in navItems"
+                :key="item.name"
+                class="nav-item"
+                @click="navigate(item)"
+                :class="{ active: route.name === item.name }"
+            >
+                {{ item.label }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style>
-.navbar-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 12vh;
-}
-
 .navbar {
     height: 50px;
     border-radius: 50px;
@@ -61,8 +52,7 @@
 </style>
 
 <script setup>
-import topbar from '../components/topbar.vue';
-import { useRouter, useRoute} from 'vue-router';
+import { useRouter, useRoute } from "vue-router";
 const route = useRoute();
 const router = useRouter();
 
--- src/components/profileIcon.vue
+++ src/components/profileIcon.vue
@@ -1,7 +1,49 @@
 <template>
-    
+    <v-btn icon class="profile-icon" @click="toggled = true">
+        <v-icon>mdi-account</v-icon>
+    </v-btn>
+
+
+    <!-- if this gets bigger lets put this in a SFC (davey) -->
+    <v-dialog v-model="toggled" width="30vw" height="30vh" >
+        <v-card class="popup">
+            <div class="centered-column">
+                <div class="popup-title">Coach Jones</div>
+                <v-btn class="logout-button" variant="tonal">Logout</v-btn>
+            </div>
+        </v-card>
+    </v-dialog>
 </template>
 
 <style scoped>
+.profile-icon {
+    position: absolute;
+    right: 3vw;
+    background-color: #d0d0d0;
+}
 
+
+/* poup css starts here */
+.centered-column {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.popup {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.popup-title {
+    font-size: 30px;
+    font-weight: 600;
+}
 </style>
+
+<script setup>
+import { ref } from "vue";
+const toggled = ref(false);
+
+</script>

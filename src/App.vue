e98ed7bfcebfa46b0bd8865ba5df7fbc0908eacf
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-13 17:04:31 -0600

    for now

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <v-app>
         <topbar>
-      <navbar tab="/dashboardCoach" v-if="route.path != '/login'" />
-        <logout class="logout" v-if="route.path != '/login'" />
-          <profileIcon/>
-    </topbar>
-    <v-main>
+            <navbar tab="/dashboardCoach" v-if="route.path != '/login'" />
+            <logout class="logout" v-if="route.path != '/login'" />
+            <profileIcon />
+        </topbar>
+        <v-main>
             <router-view />
         </v-main>
     </v-app>
@@ -13,16 +13,16 @@
 
 <style>
 .logout {
-  position: absolute;
-  left: auto;
-  right: 40px;
-  top: 40px;
+    position: absolute;
+    left: auto;
+    right: 40px;
+    top: 40px;
 }
 </style>
 
 <script setup>
-  import navbar from './components/navbar.vue';
-  import { useRoute } from 'vue-router';
+import navbar from "./components/navbar.vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 </script>

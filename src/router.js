10231cf4252ffdedce2313b69de133b5f3217cd7
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2025-11-13 16:24:59 -0600

    Fix router naming schemes

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import dashboardCoach from "./pages/coach/dashboard-coach.vue";
 import workoutsCoach from "./pages/coach/workouts-coach.vue";
 import exercisesCoach from "./pages/coach/exercises-coach.vue";
 import athletesCoach from "./pages/coach/athletes-coach.vue";
-import DashboardAthlete from "./pages/athlete/dashboard-athlete.vue";
+import dashboardAthlete from "./pages/athlete/dashboard-athlete.vue";
 
 const router = createRouter({
     //removes the # from the url
@@ -48,7 +48,7 @@ const router = createRouter({
         {
             path: "/dashboardAthlete",
             name: "dashboardAthlete",
-            component: DashboardAthlete,
+            component: dashboardAthlete,
         }
     ],
 });

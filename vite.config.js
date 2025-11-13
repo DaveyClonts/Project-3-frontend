3a889892e7015797f971bc6c9f0e231bd90ac7c7
James-Wiley <james.wiley@live.com>
2025-11-13 16:27:36 -0600

    trying a new solution

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import dns from "dns";
 dns.setDefaultResultOrder("verbatim");
 
 export default () => {
+    const baseURL =
+    process.env.APP_ENV === "development" ? "/" : "/seiv2025/p3/t6";
+    
     return defineConfig({
         plugins: [vue(), vuetify({ autoImport: true })],
         server: {

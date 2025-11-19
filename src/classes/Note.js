a64358592c30f6b625ac6bd89df1739a6655bbd5
Maximus253 <m.jones@eagles.oc.edu>
2025-11-19 01:43:46 -0600

    add

--- src/classes/Note.js
+++ src/classes/Note.js
@@ -1,7 +1,8 @@
 export default class Note {
-    constructor(note, date = new Date().toISOString().split("T")[0], noteID = null) {
-        this.noteID = noteID
+    constructor(note, goalID, date = new Date().toISOString().replace("T", " ").substring(0, 16), id = null) {
+        this.id = id;
         this.note = note;
         this.date = date;
+        this.goalID = goalID
     }
 }

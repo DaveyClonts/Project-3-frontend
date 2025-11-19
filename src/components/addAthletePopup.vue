e86fbf454dd2b337e2d914a599ba4e760d52cedb
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-18 19:42:03 -0600

    wip

--- src/components/addAthletePopup.vue
+++ src/components/addAthletePopup.vue
@@ -1,13 +1,26 @@
 <template>
-    <v-dialog max-width="500">
-        <v-card>
-            test
+    <v-dialog width="30vw" height="45vh">
+        <v-card style="display: flex;">
+            <div class="centered-column">
+                list of all athletes
+            </div>
         </v-card>
     </v-dialog>
 </template>
 
 <style scoped>
+.centered-column {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 20px;
+}
 
+.athlete-name {
+    font-size: 24px;
+    font-weight: 600;
+    padding: 10px;
+}
 </style>
 
 <script setup>

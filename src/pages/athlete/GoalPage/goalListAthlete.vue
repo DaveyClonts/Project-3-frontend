d4f428a428e60ef42fe57a5ab0a2d5217ae7c313
Maximus253 <m.jones@eagles.oc.edu>
2025-11-19 01:51:05 -0600

    fix

--- src/pages/athlete/GoalPage/goalListAthlete.vue
+++ src/pages/athlete/GoalPage/goalListAthlete.vue
@@ -22,4 +22,35 @@ const props = defineProps({
 });
 </script>
 
-<style></style>
+<style scoped>
+.builder-container {
+  height: 80vh;
+  width: 30%;
+  background-color: #d0d0d0;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+.subcontainer {
+  width: 80%;
+  gap: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: auto scroll;
+  overflow-y: auto;
+}
+
+.title {
+  border-width: 0px 0px 2px 0px;
+  border-color: rgb(82, 82, 82);
+  border-style: solid;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 6px;
+  padding-bottom: 12px;
+  width: 100%;
+}
+
+</style>

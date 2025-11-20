a7f364ac66673737473551a1398ba7438407261b
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-19 18:04:18 -0600

    logout function

--- src/components/profileIcon.vue
+++ src/components/profileIcon.vue
@@ -9,7 +9,7 @@
         <v-card class="popup">
             <div class="centered-column">
                 <div class="popup-title">{{ user.getFullName() }}</div>
-                <v-btn class="logout-button" variant="tonal">Logout</v-btn>
+                <v-btn @click="logout" class="logout-button" variant="tonal">Logout</v-btn>
             </div>
         </v-card>
     </v-dialog>
@@ -19,14 +19,41 @@
 import { ref } from "vue";
 import User from "../classes/User";
 import store from "../store/store";
-const toggled = ref(false);
+import authServices from "../services/authServices";
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const toggled = ref(false);
 const user = new User( 
     store.getUser().firstName,
     store.getUser().lastName,
     store.getUser().token,
 );
 
+function logout() {
+    console.log("Log out.");
+
+    const user = store.getUser();
+
+    if (user == null) {
+        console.log("User is null!");
+        return;
+    }
+
+    authServices
+        .logoutUser(user.token)
+        .then((response) => {
+            console.log(
+                `Successfully logged out user: ${response.data.message}`
+            );
+            
+            store.clearUser();
+            router.push({ name: "login" });
+        })
+        .catch((err) => {
+            console.log(`Error logging out user: ${err}.`);
+        });
+}
 </script>
 
 <style scoped>

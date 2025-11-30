51560131367ca65026738de2128b9b60ed1cc1df
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2025-11-30 14:46:48 -0600

    wrapped up

--- src/components/addAthletePopup.vue
+++ src/components/addAthletePopup.vue
@@ -1,18 +1,29 @@
 <template>
     <v-dialog width="30vw" height="45vh" eager>
-        <v-card class="card" style="display: flex;">
+        <v-card class="card" style="display: flex">
             <div class="centered-column">
-                <ul>
-                    <li v-for="athlete in availableAthletes">{{athlete.getFullName()}}</li>
-                </ul>
+                <v-card class="item" v-for="athlete in availableAthletes">
+                    <div class="row">
+                        <div class="checkbox">
+                            <v-checkbox-btn></v-checkbox-btn>
+                        </div>
+                            
+                        <div class="name">
+                            {{ athlete.getFullName() }}
+                        </div>
+                    </div>
+                </v-card>
+
+                <v-btn class="confirm-button">
+                    Confirm
+                </v-btn>
             </div>
         </v-card>
     </v-dialog>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue';
-
-import userServices from '../services/userServices';
+import { ref, onMounted } from "vue";
+import userServices from "../services/userServices";
 
 const availableAthletes = ref([]);
 
@@ -20,7 +31,6 @@ onMounted(async () => {
     availableAthletes.value = await userServices.getAllWithRole("athlete");
     console.log(availableAthletes.value);
 });
-
 </script>
 
 <style scoped>
@@ -33,5 +43,50 @@ onMounted(async () => {
 
 .card {
     border-radius: 40px !important;
+    justify-content: center;
+    align-items: center;
+}
+
+.item {
+    background-color: var(--color-bg);
+    width: 20vw;
+    height: 5vh;
+    border-radius: 30px;
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+}
+
+.row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.checkbox {
+    /* background-color: var(--color-primary);
+    width: 2.75vw;
+    height: 4vh;
+    border-radius: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center; */
+}
+
+.name {
+    color: var(--color-text-primary);
+    padding-left: 2vw;
+    font-size: 18px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+}
+
+.confirm-button {
+    background-color: var(--btn-secondary);
+    color: var(--btn-secondary-text);
+    border-radius: 40px;
+    min-width: 5vh;
+    height: 5vh;
 }
 </style>
--- src/pages/coach/dashboard/athletes-card.vue
+++ src/pages/coach/dashboard/athletes-card.vue
@@ -1,24 +1,36 @@
 <template>
     <v-card class="athletes-card">
         <div class="title">Your Athletes</div>
-        <div class="column">
 
-            <athlete v-for="athlete in athletes"> 
+        <div class="populated" v-if="athletes.length > 0">
+            <athlete v-for="athlete in athletes">
                 {{ athlete.getFullName() }}
             </athlete>
+            <athleteButton> View Athletes </athleteButton>
+        </div>
 
-            <addButton/>
+        <div class="no-athletes" v-if="athletes.length == 0">
+            <div class="text">You have no athletes assigned...</div>
+            <v-btn
+                variant="text"
+                class="button-none"
+                @click="router.push('/athletesCoach')"
+            >
+                Assign athletes?
+            </v-btn>
         </div>
     </v-card>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import athlete from '../../../components/athlete.vue';
-import addButton from '../../../components/editButton.vue';
-import userServices from '../../../services/userServices';
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import athlete from "../../../components/athlete.vue";
+import athleteButton from "../../../components/athleteButton.vue";
+import userServices from "../../../services/userServices";
 
 const athletes = ref([]);
+const router = useRouter();
 
 onMounted(async () => {
     athletes.value = await userServices.getAthletesForCoach(1);
@@ -33,16 +45,42 @@ onMounted(async () => {
     padding: 20px;
 }
 
-.column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 15px;
-}
-
 .title {
     font-size: 25px;
     font-weight: 600;
     padding-bottom: 10px;
+}
+
+.no-athletes {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 60vh;
+    width: 15vw;
+}
+
+.populated {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2vh;
+}
+
+.text {
+    font-size: 18px;
+    font-weight: 400;
+    color: var(--color-text-secondary);
+}
+
+.button-none {
+    color: var(--color-text-secondary);
+    text-transform: none;
+    font-weight: 400;
+    font-size: 18px;
+    letter-spacing: normal;
+    text-decoration: underline;
 }
 </style>

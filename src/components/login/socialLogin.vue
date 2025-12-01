4b4cde850cd174f578dc0acf0a84effc5970221b
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2025-12-01 04:40:15 -0600

    Add role routing (#82)

--- src/components/login/socialLogin.vue
+++ src/components/login/socialLogin.vue
@@ -12,6 +12,7 @@ import authServices from "../../services/authServices.js";
 import User from "../../classes/User.js";
 import store from "../../store/store.js";
 import { useRouter } from "vue-router";
+import userRole from "../../classes/userRole.js";
 
 const user = ref({});
 const router = useRouter();
@@ -39,7 +40,7 @@ async function loginWithGoogle() {
             size: "large",
             text: "signup_with",
             width: 300,
-            height: 200
+            height: 200,
         }
     );
 }
@@ -62,7 +63,10 @@ async function handleCredentialResponse(response) {
 
             console.log("Successfully logged in.");
             store.setUser(user.value);
-            router.push({ name: "dashboardCoach" });
+
+            if (user.role == userRole.Athlete)
+                router.push({ name: "dashboardAthlete" });
+            else router.push({ name: "dashboardCoach" });
         })
         .catch((err) => {
             console.error(`Error with authentication: ${err}.`);

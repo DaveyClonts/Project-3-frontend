da4e96a7e3eab7c0b1e59b05e8d6caf146de1329
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-12-01 03:15:35 -0600

    Add athlete data table

--- src/pages/coach/athletes-coach.vue
+++ src/pages/coach/athletes-coach.vue
@@ -1,3 +1,49 @@
 <template>
-    Athletes View
+    <v-card class="top-container rounded-xl">
+        <v-data-table :headers="headers" :items="athletes" hide-default-footer>
+            <template v-slot:item.owned="{ item }">
+                <v-checkbox-btn class="toggle-button" v-model="item.owned"> </v-checkbox-btn>
+            </template>
+        </v-data-table>
+    </v-card>
 </template>
+
+<style>
+.top-container {
+    margin-top: 8px;
+    width: 500px;
+    padding: 20px;
+    height: 80vh;
+    background-color: var(--color-primary);
+    justify-content: center;
+    justify-self: center;
+}
+
+.toggle-button {
+    color: var(--btn-primary);
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+import userRole from "../../classes/userRole";
+import userServices from "../../services/userServices";
+
+const athletes = ref([]);
+
+const headers = [
+    {
+        title: "Name",
+        key: "name",
+        value: (item) => `${item.firstName} ${item.lastName}`,
+    },
+    {
+        title: "Owned",
+        key: "owned",
+    },
+];
+
+userServices.getAllWithRole(userRole.Athlete).then((users) => {
+    athletes.value = users;
+});
+</script>

0b50d4c56525e4f5467a21ba82d71ee1a1abbfa3
Maximus253 <122314662+Maximus253@users.noreply.github.com>
2025-12-03 23:40:01 -0600

    107 fix athlete dashboard (#116)
    * fix
    
    * fixed
    
    * fix

--- src/services/goalServices.js
+++ src/services/goalServices.js
@@ -70,9 +70,9 @@ export default {
 /**
    * @returns {Promise<Goal>}
    */
-  async delete(goalID) {
+  async delete(id) {
     try {
-      return apiClient.delete(`${API_ROOT}/${goalID}`);
+      return apiClient.delete(`${API_ROOT}/${id}`);
     } catch (error) {
       console.error("Error creating course:", error);
       return null;

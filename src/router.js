f33aae0b9f1a1fad15fec58f491e8ae2d3681bcc
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2025-12-03 18:38:47 -0600

    Rc aws fixes (#106)
    * Add file extensions
    
    * Capitalize file path
    
    * Fix null user reference
    
    * Add route logging
    
    * Add more router logging
    
    * Fix router base URL
    
    * Update base url initialization
    
    * Update router.js
    
    * Change to hash URLs
    
    * Add quotes to deploy
    
    * Remove router log

--- src/router.js
+++ src/router.js
@@ -91,8 +91,6 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    console.log(`From: ${JSON.stringify(from)}, to: ${JSON.stringify(to)}`)
-    
     if (!to.meta.requiresAuth) {
         next();
         return;
